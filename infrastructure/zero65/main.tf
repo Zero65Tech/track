384acb26f1fec5927c14c94af85ec27693201a42
Anika Gupta <anikagupta0316@gmail.com>
2025-12-07 03:53:26 +0000

    prod infrastructure

--- infrastructure/zero65/main.tf
+++ infrastructure/zero65/main.tf
@@ -2,25 +2,46 @@ terraform {
   cloud { 
     organization = "Zero65Tech" 
     workspaces { 
-      name = "zero65" 
+      name = "track_zero65" 
     } 
   } 
 }
 
 module "track-v4" {
-  source     = "../module"
-  project_id = "zero65" 
-  region     = "asia-south1"
-  zone       = "asia-south1-a"
-  name       = "track-v4"
-  stage      = "prod"
+  source      = "../module"
+  name        = "track-v4"
+  stage       = "prod"
+  github      = {
+    org = "Zero65Tech"
+    repo = "track-v4"
+  }
+  project_id  = "zero65"
+  region      = "asia-south1"
+  zone        = "asia-south1-a"
 }
 
-module "paisa-v4" {
-  source     = "../module"
-  project_id = "zero65" 
-  region     = "asia-south1"
-  zone       = "asia-south1-a"
-  name       = "paisa-v4"
-  stage      = "prod"
+module "track-v5" {
+  source      = "../module"
+  name        = "track-v5"
+  stage       = "prod"
+  github      = {
+    org = "Zero65Tech"
+    repo = "track"
+  }
+  project_id  = "zero65"
+  region      = "asia-south1"
+  zone        = "asia-south1-a"
+}
+
+module "paisa-v5" {
+  source      = "../module"
+  name        = "paisa-v5"
+  stage       = "prod"
+  github      = {
+    org = "Zero65Tech"
+    repo = "track"
+  }
+  project_id  = "zero65"
+  region      = "asia-south1"
+  zone        = "asia-south1-a"
 }
