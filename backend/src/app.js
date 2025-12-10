cb0e9ef6aee0f055959dcbfd5512f86276339cd3
Anika Gupta <anikagupta0316@gmail.com>
2025-12-10 04:29:16 +0000

    test

--- backend/src/app.js
+++ backend/src/app.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 
 app.get(`/api/ls`, (req, res) => {
   try {
-    const files = fs.readdirSync(".");
+    const files = fs.readdirSync("./backend");
     res.json({
       cwd: process.cwd(),
       files,
