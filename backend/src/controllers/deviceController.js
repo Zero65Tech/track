bc04618790f2dd7e5f3d1e9c12dfb8058eef85ae
Prashant Gupta <antshpra@gmail.com>
2025-12-20 05:12:53 +0000

    controller

---
+++ backend/src/controllers/deviceController.js
@@ -0,0 +1,38 @@
+import { createDeviceSchema, updateDeviceSchema } from "@shared/schemas";
+import {
+  sendData,
+  sendSuccess,
+  sendBadRequestError,
+} from "../utils/response.js";
+import deviceService from "../services/deviceService.js";
+
+async function createDevice(req, res) {
+  const { success, error, data } = createDeviceSchema.safeParse(req.body);
+  if (!success) {
+    return sendBadRequestError(res, error);
+  }
+
+  const device = await deviceService.createDevice(req.user.uid, data.fcmToken);
+
+  return sendData(res, device);
+}
+
+async function updateDevice(req, res) {
+  const { success, error, data } = updateDeviceSchema.safeParse(req.body);
+  if (!success) {
+    return sendBadRequestError(res, error);
+  }
+
+  const device = await deviceService.updateDevice(
+    req.user.uid,
+    req.params.id,
+    data.fcmToken,
+  );
+
+  return sendSuccess(res, device);
+}
+
+export default {
+  createDevice,
+  updateDevice,
+};
