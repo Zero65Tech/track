0b982812e37c15974cf02fb78f8601474f52b5ac
Prashant Gupta <antshpra@gmail.com>
2025-12-26 06:45:51 +0000

    shorter instance id

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,14 @@
 import dotenv from "dotenv";
-import { randomUUID } from "crypto";
+import { randomBytes } from "crypto";
 
 import { initialiseFirebase } from "./config/firebase.js";
 import { connectToDatabase } from "./config/db.js";
 import cron from "./cron.js";
 import app from "./app.js";
 
+const instanceId = randomBytes(4).toString("hex");
+
 (async () => {
-  const instanceId = randomUUID();
   dotenv.config({
     path: [
       ".env",
