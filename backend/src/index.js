a8b89fcd51e4b64dca1953785347c801c76af046
Prashant Gupta <antshpra@gmail.com>
2026-01-25 15:24:06 +0000

    Minor refactoring

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,26 @@
 import dotenv from "dotenv";
 import { randomBytes } from "crypto";
-
 import { initialiseFirebase } from "./config/firebase.js";
 import { connectToDatabase } from "./config/db.js";
 import cron from "./cron.js";
 import app from "./app.js";
 
-const instanceId = randomBytes(4).toString("hex");
+dotenv.config({
+  path: [
+    ".env",
+    `.env.${process.env.STAGE}`,
+    `.env.${process.env.STAGE}.local`,
+  ],
+});
 
-(async () => {
-  dotenv.config({
-    path: [
-      ".env",
-      `.env.${process.env.STAGE}`,
-      `.env.${process.env.STAGE}.local`,
-    ],
-  });
-  initialiseFirebase();
-  await connectToDatabase();
-  cron.start(instanceId);
-  app.listen(process.env.PORT, () =>
-    console.log(
-      `🎉 Server (${process.env.STAGE}) is listening on port ${process.env.PORT} [instance: ${instanceId}]`,
-    ),
-  );
-})();
+initialiseFirebase();
+await connectToDatabase();
+
+const instanceId = randomBytes(4).toString("hex");
+cron.start(instanceId);
+
+app.listen(process.env.PORT, () =>
+  console.log(
+    `🎉 Server (${process.env.STAGE}) is listening on port ${process.env.PORT} [instance: ${instanceId}]`,
+  ),
+);
