4851dc9b09caa51f41c37604490e7498a035e6b9
disha-0114 <gupta.disha30@gmail.com>
2026-02-21 05:22:47 +0000

    expenses_by_tag_month

---
+++ backend/src/config/aggregations/expenses_by_tag_month.js
@@ -0,0 +1,45 @@
+import { EntryType } from "@shared/enums";
+
+export default (profileId) => [
+  {
+    $match: {
+      profileId,
+      type: {
+        $in: [EntryType.EXPENSE.id, EntryType.REFUND.id],
+      },
+    },
+  },
+  {
+    $addFields: {
+      date: {
+        $dateFromString: { dateString: "$date" },
+      },
+    },
+  },
+  {
+    $group: {
+      _id: {
+        tagId: "$tagId",
+        month: {
+          $dateToString: {
+            format: "%Y-%m",
+            date: "$date",
+          },
+        },
+      },
+      balance: {
+        $sum: {
+          $cond: [
+            { $eq: ["$type", EntryType.EXPENSE.id] },
+            "$amount",
+            { $multiply: ["$amount", -1] },
+          ],
+        },
+      },
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $sort: { "_id.tagId": 1, "_id.month": 1 },
+  },
+];
