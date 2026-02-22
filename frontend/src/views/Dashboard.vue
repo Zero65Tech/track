13d4966dd43123c49af1bc6bb1115349ef79acae
disha-0114 <gupta.disha30@gmail.com>
2026-02-22 16:57:56 +0000

    refactor

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,11 +1,11 @@
 <script setup>
 import AmountsByTypeBookMonthWidget from '@/components/dashboard/AmountsByTypeBookMonthWidget.vue';
+import BalancesByWeekWidget from '@/components/dashboard/BalancesByWeekWidget.vue';
 import BestSellingWidget from '@/components/dashboard/BestSellingWidget.vue';
 import NotificationsWidget from '@/components/dashboard/NotificationsWidget.vue';
 import RecentSalesWidget from '@/components/dashboard/RecentSalesWidget.vue';
 import RevenueStreamWidget from '@/components/dashboard/RevenueStreamWidget.vue';
 import StatsWidget from '@/components/dashboard/StatsWidget.vue';
-import WeeklyBalancesWidget from '@/components/dashboard/WeeklyBalancesWidget.vue';
 import { EntryType } from '@shared/enums';
 </script>
 
@@ -13,11 +13,11 @@ import { EntryType } from '@shared/enums';
     <div class="grid grid-cols-12 gap-8">
         <StatsWidget />
 
-        <WeeklyBalancesWidget />
+        <BalancesByWeekWidget />
 
-        <AmountsByTypeBookMonthWidget title="Credits - Debits" :entry-types="[EntryType.CREDIT.id, EntryType.DEBIT.id]" />
-        <AmountsByTypeBookMonthWidget title="Incomes - Taxes" :entry-types="[EntryType.INCOME.id, EntryType.TAX.id]" />
         <AmountsByTypeBookMonthWidget title="Expenses - Refunds" :entry-types="[EntryType.EXPENSE.id, EntryType.REFUND.id]" />
+        <AmountsByTypeBookMonthWidget title="Incomes - Taxes" :entry-types="[EntryType.INCOME.id, EntryType.TAX.id]" />
+        <AmountsByTypeBookMonthWidget title="Credits - Debits" :entry-types="[EntryType.CREDIT.id, EntryType.DEBIT.id]" />
 
         <div class="col-span-12 xl:col-span-6">
             <RecentSalesWidget />
