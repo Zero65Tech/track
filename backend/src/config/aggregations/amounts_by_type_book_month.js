7d8d55e453ca91fa6c8e67635a0027bb068d12e6
disha-0114 <gupta.disha30@gmail.com>
2026-02-22 07:10:58 +0000

    amounts_by_type_book_month aggregation

---
+++ backend/src/config/aggregations/amounts_by_type_book_month.js
@@ -0,0 +1,55 @@
+import { EntryType } from "@shared/enums";
+
+export default (profileId) => [
+  {
+    $match: {
+      profileId,
+      type: {
+        $in: [
+          EntryType.CREDIT.id,
+          EntryType.DEBIT.id,
+          EntryType.INCOME.id,
+          EntryType.EXPENSE.id,
+          EntryType.REFUND.id,
+          EntryType.TAX.id,
+        ],
+      },
+    },
+  },
+  {
+    $addFields: {
+      date: {
+        $dateFromString: { dateString: "$date" },
+      },
+    },
+  },
+  {
+    $group: {
+      _id: {
+        month: {
+          $dateToString: {
+            format: "%Y-%m",
+            date: "$date",
+          },
+        },
+        type: "$type",
+        bookId: "$bookId",
+      },
+      balance: {
+        $sum: {
+          $cond: [
+            {
+              $in: ["$type", [EntryType.CREDIT.id, EntryType.INCOME.id, EntryType.REFUND.id]],
+            },
+            "$amount",
+            { $multiply: ["$amount", -1] },
+          ],
+        },
+      },
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $sort: { "_id.month": 1, "_id.type": 1, "_id.bookId": 1 },
+  },
+];
