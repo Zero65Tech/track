db52bd2071026be9a915d8cb4b256369053edaae
disha-0114 <gupta.disha30@gmail.com>
2026-02-26 03:19:49 +0000

    aggregation controller

--- backend/src/controllers/aggregationController.js
+++ backend/src/controllers/aggregationController.js
@@ -5,20 +5,14 @@ import aggregationService from "../services/aggregationService.js";
 // Named
 
 async function getNamedAggregationResult(req, res) {
-  const data = await aggregationService.getNamedAggregation(
-    req.params.profileId,
-    req.params.name,
-  );
-  sendData(res, data ? { result: data.result, timestamp: data.updatedAt } : {});
+  const data = await aggregationService.getNamedAggregation(req.params.profileId, req.params.name);
+  sendData(res, { result: data?.result || [], timestamp: data?.updatedAt || null });
 }
 
 // Custom
 
 async function getCustomAggregationResult(req, res) {
-  const result = await aggregationService.getCustomAggregation(
-    req.params.profileId,
-    req.params.id,
-  );
+  const result = await aggregationService.getCustomAggregation(req.params.profileId, req.params.id);
   sendData(res, { result });
 }
 
@@ -28,10 +22,7 @@ async function createCustomAggregation(req, res) {
     return sendBadRequestError(res, error);
   }
 
-  const result = await aggregationService.createCustomAggregation(
-    req.params.profileId,
-    req.body,
-  );
+  const result = await aggregationService.createCustomAggregation(req.params.profileId, req.body);
   sendData(res, { result });
 }
 
