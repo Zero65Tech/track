59ddf2ecd914b40de30bc49e5d289f4267d40044
disha-0114 <gupta.disha30@gmail.com>
2026-06-18 14:03:49 +0000

    [FE] Refactoring book store and service

--- frontend/src/service/bookService.js
+++ frontend/src/service/bookService.js
@@ -1,7 +1,15 @@
 import apiClient from '@/service/apiClient';
 
 export const bookService = {
-    async getBooks({ profileId }, abortControllerSignal) {
-        return (await apiClient.get(`/profiles/${profileId}/books`, { signal: abortControllerSignal })).data.data.books;
+    async getBooks(profileId) {
+        const apiResponse = await apiClient.get(`/profiles/${profileId}/books`);
+
+        const apiResponseData = apiResponse.data.data;
+        for (const book of apiResponseData.books) {
+            book.createdAt = new Date(book.createdAt);
+            book.updatedAt = new Date(book.updatedAt);
+        }
+
+        return apiResponseData;
     }
 };
--- frontend/src/stores/book.store.js
+++ frontend/src/stores/book.store.js
@@ -1,19 +1,55 @@
 import { bookService } from '@/service/bookService';
 import { useProfileStore } from '@/stores/profile.store';
 import { defineStore } from 'pinia';
-import { useToast } from 'primevue/usetoast';
 import { computed, ref, watch } from 'vue';
 
 export const useBookStore = defineStore('book', () => {
-    const toast = useToast();
     const profileStore = useProfileStore();
-    let abortController = new AbortController();
+
+    let inFlightRequest = null;
 
     // States
 
     const isLoading = ref(false);
     const books = ref([]);
     const error = ref(null);
+
+    // Internal Functions
+
+    watch(
+        () => profileStore.activeProfile,
+        async () => {
+            // NOTE: This may not work as expected if the user switches profiles quickly,
+            // but it should be good enough for now. We can improve this later if needed.
+            if (inFlightRequest) {
+                await inFlightRequest;
+            }
+
+            books.value = [];
+
+            const profileId = profileStore.activeProfile?.id;
+            if (profileId) {
+                await _fetchBooks(profileId);
+            } else {
+                error.value = null;
+            }
+        }
+    );
+
+    async function _fetchBooks(profileId) {
+        isLoading.value = true;
+        error.value = null;
+
+        try {
+            const apiResponseData = await bookService.getBooks(profileId);
+            books.value = apiResponseData.books;
+        } catch (err) {
+            error.value = err.message;
+            console.log(err);
+        } finally {
+            isLoading.value = false;
+        }
+    }
 
     // Getters
 
@@ -28,48 +64,9 @@ export const useBookStore = defineStore('book', () => {
     // Actions
 
     async function initialize() {
-        if (profileStore.activeProfile) {
-            await fetchBooks();
-        }
-    }
-
-    watch(
-        () => profileStore.activeProfile,
-        () => {
-            // Abort all in-flight requests
-            abortController.abort();
-            abortController = new AbortController();
-            if (profileStore.activeProfile) {
-                fetchBooks();
-            } else {
-                books.value = [];
-                error.value = null;
-            }
-        }
-    );
-
-    async function fetchBooks() {
         const profileId = profileStore.activeProfile?.id;
-        if (!profileId) {
-            toast.add({
-                severity: 'error',
-                summary: 'Refresh failed',
-                detail: 'Kindly select a profile to fetch books',
-                life: 3000
-            });
-            return;
-        }
-
-        isLoading.value = true;
-        error.value = null;
-
-        try {
-            books.value = await bookService.getBooks({ profileId }, abortController.signal);
-        } catch (err) {
-            error.value = err.message;
-            console.log(err);
-        } finally {
-            isLoading.value = false;
+        if (profileId) {
+            await _fetchBooks(profileId);
         }
     }
 
@@ -83,7 +80,6 @@ export const useBookStore = defineStore('book', () => {
         booksMap,
 
         // Actions
-        initialize,
-        fetchBooks
+        initialize
     };
 });
